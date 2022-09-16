03d5b50b50b25a47de54946004b6f24d47e18169
Vitoria Pacheco <vitoriac.pac@gmail.com>
2022-09-15 21:03:28 -0300

    Stage 1/6 | Base Building

---
+++ server/src/server.ts
@@ -0,0 +1,15 @@
+import express from 'express'
+
+const app = express()
+
+// localhost:3333/ads
+app.get('/ads', (request, response) => {
+  return response.json([
+    { id: 1, name: 'Ad 1' },
+    { id: 2, name: 'Ad 2' },
+    { id: 3, name: 'Ad 3' },
+    { id: 4, name: 'Ad 4' }
+  ])
+})
+
+app.listen(3333)
